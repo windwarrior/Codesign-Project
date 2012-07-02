e9415ad8783db87f69a69bf6cc78e294ab60e80d
Nick Schot <nickschot@gmail.com>
2012-07-02 14:43:58 +0200

    Opmaak code compassLib

--- Eindopdracht/compassLib/Compass.cpp
+++ Eindopdracht/compassLib/Compass.cpp
@@ -12,14 +12,14 @@ double Compass::getHeading(){
 	// Send a "A" command to the HMC6352
 	// This requests the current heading data
 	Wire.beginTransmission(slaveAddress);
-	Wire.write("A");              // The "Get Data" command
+	Wire.write("A");			// The "Get Data" command
 	Wire.endTransmission();
-	delay(10);                   // The HMC6352 needs at least a 70us (microsecond) delay
-	// after this command.  Using 10ms just makes it safe
-	// Read the 2 heading bytes, MSB first
-	// The resulting 16bit word is the compass heading in 10th's of a degree
-	// For example: a heading of 1345 would be 134.5 degrees
-	Wire.requestFrom(slaveAddress, 2);        // Request the 2 byte heading (MSB comes first)
+	delay(10);					// The HMC6352 needs at least a 70us (microsecond) delay
+								// after this command.  Using 10ms just makes it safe
+								// Read the 2 heading bytes, MSB first
+								// The resulting 16bit word is the compass heading in 10th's of a degree
+								// For example: a heading of 1345 would be 134.5 degrees
+	Wire.requestFrom(slaveAddress, 2);	// Request the 2 byte heading (MSB comes first)
 	i = 0;
 	while(Wire.available() && i < 2)
 	{ 
